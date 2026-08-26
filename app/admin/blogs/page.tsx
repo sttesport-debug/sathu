50f0d6e875a4d04a2e6c69ebcdf02a6feb28d9d4
Nikanya Raksasat <6729011007@cdti.ac.th>
2026-08-26 15:13:52 +0700

    update

--- app/admin/blogs/page.tsx
+++ app/admin/blogs/page.tsx
@@ -1,9 +1,11 @@
 import BlogForm from "@/components/BlogForm";
+import BlogCard from "@/components/BlogCard";
 
 export default function BlogPage() {
     return (
         <div>
-           <BlogForm/>
+            <BlogForm />
+            <BlogCard />
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import BlogCard from "@/components/BlogCard";
+import Impage from "next/image";
 
 
 export default function Home() {
@@ -5,6 +7,9 @@ export default function Home() {
     <div >
       <h2> ยินดีต้อนรับเข้าสู่เว็บไซต์ </h2>
       <p> โดย ปรมะ เกิดผลหลาก </p>
+
+      <BlogCard/>
+
     </div>
   );
 }
